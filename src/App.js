488216128195b689f9817234114fc9f4dc4cf59b
mh1369080 <mh1369080@gmail.com>
2023-07-30 16:37:06 +0800

    Update Website

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ export const Desktop = () => {
         <div className="overlap-group">
           <div className="rectangle" />
           <div className="div" />
-          <div className="frame">
+          <button className="frame">
             <div className="text-wrapper">Upload CV</div>
-          </div>
+          </button>
           <div className="rectangle-2" />
           <div className="rectangle-wrapper">
             <div className="rectangle-3" />
           </div>
           <div className="rectangle-4" />
           <div className="rectangle-5" />
+          <button className="polygon-button">
           <img className="polygon" alt="Polygon" src="polygon-1.svg" />
+          </button>
           <div className="rectangle-6" />
           <div className="rectangle-7" />
           <p className="p">Hello, are you looking for a job?</p>
           <div className="text-wrapper-2">Yes!</div>
-          <div className="input">
-            <div className="text-wrapper-3">Message</div>
+          <div className="box">
+            <div className="rectangle-wrapper">
+                <div className="rectangle" />
+            </div>
           </div>
         </div>
       </div>
