dcdea0180558ffbf9df8536faac15358af899463
mh1369080 <mh1369080@gmail.com>
2023-07-30 15:58:21 +0800

    Update Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <div className="div" />
+          <div className="frame">
+            <div className="text-wrapper">Upload CV</div>
+          </div>
+          <div className="rectangle-2" />
+          <div className="rectangle-wrapper">
+            <div className="rectangle-3" />
+          </div>
+          <div className="rectangle-4" />
+          <div className="rectangle-5" />
+          <img className="polygon" alt="Polygon" src="polygon-1.svg" />
+          <div className="rectangle-6" />
+          <div className="rectangle-7" />
+          <p className="p">Hello, are you looking for a job?</p>
+          <div className="text-wrapper-2">Yes!</div>
+          <div className="input">
+            <div className="text-wrapper-3">Message</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Desktop />
     </div>
   );
 }
